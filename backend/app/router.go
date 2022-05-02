5e0326d5288526c1b843d8abeae8051f60f7ff37
StudentOfJS <rodlewis45@gmail.com>
2022-05-02 14:25:58 +0800

    login

--- backend/app/router.go
+++ backend/app/router.go
@@ -4,7 +4,7 @@ import "basic_auth/backend/controller/users"
 
 func mapUrls() {
 	router.POST("/api/register", users.Register)
-	// router.POST("/api/login", users.Login)
+	router.POST("/api/login", users.Login)
 	router.GET("/api/user", users.Get)
 	// router.GET("/api/logout", users.Logout)
 }
