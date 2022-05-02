305b0a6a2d33de14eff409f749480f06e08d78f0
StudentOfJS <rodlewis45@gmail.com>
2022-05-02 14:37:09 +0800

    cors

--- backend/app/router.go
+++ backend/app/router.go
@@ -1,8 +1,24 @@
 package app
 
-import "basic_auth/backend/controller/users"
+import (
+	"basic_auth/backend/controller/users"
+	"time"
+
+	"github.com/gin-contrib/cors"
+)
 
 func mapUrls() {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	router.POST("/api/register", users.Register)
 	router.POST("/api/login", users.Login)
 	router.GET("/api/user", users.Get)
